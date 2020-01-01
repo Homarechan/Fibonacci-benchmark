6a25be2624d4719ff921b1bf8fff15762c7f27f9
Homarechan <tikuwakunn@icloud.com>
2020-01-01 14:33:48 +0900

    Fix bug in the Erlang code

--- erlang/main.erl
+++ erlang/main.erl
@@ -1,10 +1,10 @@
 -module(main).
 -export([do_main/0]).
 
-fibonacci(0) -> 1;
 fibonacci(1) -> 1;
+fibonacci(2) -> 1;
 fibonacci(N) -> fibonacci(N-1) + fibonacci(N-2).
 
-do_main() -> io:format("~p\n", fibonacci(40)).
+do_main() -> io:format("~p\n", [fibonacci(40)]).
 
 
